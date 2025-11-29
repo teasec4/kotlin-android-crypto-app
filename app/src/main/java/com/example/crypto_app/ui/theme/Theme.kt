8627186053af2e32858505c039a2ff970ea78228
Kovalev Maksim <teasec4@yandex.ru>
2025-11-29 10:29:09 +0800

    system icon color fix

--- app/src/main/java/com/example/crypto_app/ui/theme/Theme.kt
+++ app/src/main/java/com/example/crypto_app/ui/theme/Theme.kt
@@ -7,7 +7,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Primary,
@@ -43,6 +47,15 @@ fun CryptoappTheme(
         DarkColorScheme
     } else {
         LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window?.statusBarColor = colorScheme.surface.toArgb()
+            WindowCompat.getInsetsController(window, view)?.isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
